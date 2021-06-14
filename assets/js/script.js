d8b9dad519bd2e7b51ca94ac46f38f813a312512
LisaHackett97 <lisa-hackett@hotmail.com>
2021-06-14 20:09:54 +0000

    game: set up initial list of fn's to use

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() { });
+
+// event listeners for click amd keydown.
+
+function runGame() {  };
+
+function displayQuestion() { };
+
+function displayAnswerChoice() { };
+
+function checkAnswer() { };
+
+function newQuestion(){ };
+
+function moveInGame() { };
+
+function reTry() { };
+
+function incrementScore() { };
