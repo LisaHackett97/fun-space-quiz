a8a1db1e35ef4930c5738d8daa6bf6c713218969
LisaHackett97 <lisa-hackett@hotmail.com>
2021-06-15 10:18:53 +0000

    js: initial set up of questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+let questions = [{
+        question: "Which is the first planet in the solar system from the sun?",
+        answers: {
+            a: "Venus",
+            b: "Saturn",
+            c: "Jupiter",
+            d: "Mercury"
+        },
+        correctAnswer: 'd'
+    },
+
+    {
+        question: "Which is the first planet in the solar system from the sun?",
+        answers: {
+            a: "It doesn't have one",
+            b: "None of these",
+            c: "The light from the sun reflects directly on the surface",
+            d: "Venus is covered with a thick layer of clouds"
+        },
+        correctAnswer: 'd'
+    },
+
+    {
+        question: "The second largest moon in our solar system is a natural satellite of Saturn. What is its name?",
+        answers: {
+            a: "Titan ",
+            b: "Europa",
+            c: "Rhea",
+            d: " Mimas"
+        },
+        correctAnswer: 'a'
+    }
+
+];
