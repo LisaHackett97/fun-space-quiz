12f3c14198c324e26d05fe301eb05682bdadc450
LisaHackett97 <lisa-hackett@hotmail.com>
2021-06-23 19:45:43 +0000

    fix: form-remove duplicate earnig on invalid email amd clear form after mail sent

--- assets/js/email.js
+++ assets/js/email.js
@@ -12,30 +12,17 @@ let contactForm = document.getElementById("contact-form");
             })
             .then(
                 function (response) {
-                    // alert("thank you so much for sending us your email")
-                    $('#emailModal').modal('show');
+                                      $('#emailModal').modal('show');
 
-                //    confirm("thank you so much for sending us your email", response)
                 },
-                function (error) {
+                 function (error) {
                    confirm("We're sorry, your message couldn't be sent at this time", error);
                 }
             );
         return false; //to block from loading a new page
     }
 
-/* 
-credit code for email validation
-https://www.w3resource.com/javascript/form/email-validation.php
-only alert user if email adress is showing as invalid
-*/
-function ValidateEmail(inputText) {
-    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (inputText.value.match(mailformat)) {
-        document.form.email.focus();
-        return true;
-    } else {
-        alert("You have entered an invalid email address!");
-        document.form.email.focus();
-        return false;
-    }}
+    // function to clear from. called when close button on email confirmation button selected
+function clearForm(){
+    document.getElementById("contact-form").reset();
+}    
